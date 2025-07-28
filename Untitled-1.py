73aeac59e38fd08dd5d98c1b9b0b7e0570a262e1
douglas jansey <douglas.jansey@gmail.com>
2025-07-28 20:00:58 -0300

    add calcIm

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,5 +1,5 @@
-age = int(input("Digite sua idade!"))
 def checkAge(age):
+    age = int(input("Digite sua idade!"))
     if age <= 0:
         print("Insira uma idade válida!")
     elif age <= 18:
@@ -7,5 +7,21 @@ def checkAge(age):
     else:
         print("Maior de idade!")
         
-checkAge(age)
 
+
+def calcImc(peso, altura):
+    peso = float(input("Digite seu peso em kg: "))
+    altura = float(input("Digite sua altura em metros: "))
+    imc = peso / (altura ** 2)
+
+    print(f"Seu IMC é: {imc:.2f}")
+
+    if imc < 18.5:
+        print("Classificação: Abaixo do peso")
+    elif imc < 25:
+        print("Classificação: Peso normal")
+    elif imc < 30:
+        print("Classificação: Sobrepeso")
+    else:
+        print("Classificação: Obeso")
+        
